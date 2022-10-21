ed446b55ff3ca0a2adba6fca1f7343b7d5b869e6
Manuel Herrera <manuel.herrera.m@gmail.com>
2022-10-21 15:55:54 -0300

    Adds Searchbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
+import SearchBar from './components/Searchbar';
 
 function App() {
   return (
-    <Navbar />
+    <Navbar />,
+    <SearchBar />
   );
 }
 
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,14 @@
+function SearchBar() {
+  return (
+    <div className="container-flex">
+      <input
+        className="form-control"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
