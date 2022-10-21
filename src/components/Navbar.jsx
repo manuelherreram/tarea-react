c5fba8b3304669187a603ba3214b70ff088fb9e6
Manuel Herrera <manuel.herrera.m@gmail.com>
2022-10-21 15:21:18 -0300

    Adds Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">Los Simpsons</span>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
